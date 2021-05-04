52554d5aacd17384bd4ff7d706f4bc8666adda0d
Wojciech Kawecki <wojtec@gmail.com>
2021-05-04 20:59:17 +0200

    add tarrif G12W

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ from pyvirtualdisplay import Display
 import paho.mqtt.client as mqtt
 import json
 import argparse
+
+def convert(text):
+    text = text.replace(" ","")
+    text = text.replace(",",".")
+    return float(text)
+
 argsparser = argparse.ArgumentParser()
-argsparser.add_argument("-tm","--type_of_meter", help="Typ licznika: 1 dla jednokierunkowego, 2 dla dwukierunkowego",type=int,choices=[1,2],required=True)
+argsparser.add_argument("-tm","--type_of_meter", help="Typ licznika: 1 dla jednokierunkowego, 2 dla dwukierunkowego, 3 dla jednokierunkowego (taryfa G12W), 4 dla dwukierunkowego (taryfa G12W)",type=int,choices=[1,2,3,4],required=True)
 argsparser.add_argument("-ms","--mqtt_server", help="Serwer MQTT",required=True)
 argsparser.add_argument("-mu","--mqtt_username", help="Uzytkownik MQTT",required=False)
 argsparser.add_argument("-mp","--mqtt_password", help="Haslo MQTT",required=False)
@@ -41,25 +47,30 @@ driver.find_element_by_name("loginNow").click()
 print("Logged...")
 try:
     element_used = driver.find_element_by_id("right").find_elements_by_tag_name("tr")[0].find_element_by_class_name("last").text
-    print(element_used)
 except:
     print("PROBLEM Z POBRANIEM DANYCH Z ENERGA S.A - SPRAWDZ DANE LOGOWANIA")
     driver.quit()
     display.stop()
     exit()
-if args.type_of_meter ==2: element_produced = driver.find_element_by_id("right").find_elements_by_tag_name("tr")[2].find_element_by_class_name("last").text
+if args.type_of_meter ==2: 
+    element_produced = driver.find_element_by_id("right").find_elements_by_tag_name("tr")[2].find_element_by_class_name("last").text
+    val_produced = convert(element_produced)
+    mqtt_msg = json.dumps({"used": val_used, "produced": val_produced})
+elif args.type_of_meter == 3:
+    element_used_1 = element_used
+    element_used_2 = driver.find_element_by_id("right").find_elements_by_tag_name("tr")[2].find_element_by_class_name("last").text
+    mqtt_msg = json.dumps({"used_1": convert(element_used_1), "used_2": convert(element_used_2)})
+elif args.type_of_meter == 4:
+    element_used_1 = element_used
+    element_used_2 = driver.find_element_by_id("right").find_elements_by_tag_name("tr")[2].find_element_by_class_name("last").text
+    element_produced_1 = driver.find_element_by_id("right").find_elements_by_tag_name("tr")[4].find_element_by_class_name("last").text
+    element_produced_2 = driver.find_element_by_id("right").find_elements_by_tag_name("tr")[6].find_element_by_class_name("last").text
+    mqtt_msg = json.dumps({"used_1": convert(element_used_1), "used_2": convert(element_used_2), "produced_1": convert(element_produced_1), "produced_2": convert(element_produced_2)})
+else: 
+    val_used = convert(element_used)
+    mqtt_msg = json.dumps({"used": val_used})
 driver.quit()
 display.stop()
-element_used = element_used.replace(" ","")
-element_used = element_used.replace(",",".")
-val_used = float(element_used)
-if args.type_of_meter ==2:
-    element_produced = element_produced.replace(" ","")
-    element_produced = element_produced.replace(",",".")
-    val_produced = float(element_produced)
-    mqtt_msg = json.dumps({"used": val_used, "produced": val_produced})
-else:
-    mqtt_msg = json.dumps({"used": val_used})
 
 print(mqtt_msg)
 
@@ -81,6 +92,5 @@ except:
 client.on_connect = on_connect
 client.loop_forever()
 
-print("Published")
-
+print("Published.")
 
